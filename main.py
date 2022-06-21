d7fc2fec914e3e1fcadcdf44e2ae6c17629bb6ad
mley714 <46652932+mley714@users.noreply.github.com>
2022-06-21 16:12:35 -0700

    testing new function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
 
+from secret import password
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -8,11 +10,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a,b):
+    return a+b
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Manuel')
     print("How are you doing?")
-    print("Where do you live?")
+    print(my_function(2,4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
