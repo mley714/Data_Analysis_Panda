b8ad3ef97fef384cfeeab712b86460ec8a51ed75
mley714 <46652932+mley714@users.noreply.github.com>
2022-06-23 17:05:30 -0700

    Updated

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pandas as pd
-from datetime import datetime
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
+
+from datetime import datetime
 # Get historical data
 
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
 raw_csv_data =pd.read_csv("foods.csv")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-df = raw_csv_data.copy()
-# print(df)
+print(raw_csv_data)
 
 
 Stock = "AAPL"
-data = yf.download(Stock, start="2022-06-01",end="2022-06-22")
+data = yf.download(Stock, start="2021-06-01",end="2022-06-22")
 data.tail(5)
 print(data)
